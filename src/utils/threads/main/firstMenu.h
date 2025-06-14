4ca5dfce51441006fe84fb1b02679b219535732d
KarlaRivasH <00107225@uca.edu.sv>
2025-06-14 17:07:20 -0600

    :construction: Comienzo basico de la creacion del menu principal Inicio del menu principal, falta diseño y mejorar lo en varios aspectos

--- src/utils/threads/main/firstMenu.h
+++ src/utils/threads/main/firstMenu.h
@@ -1,8 +1,25 @@
+#pragma once
+#include <iostream>
+using namespace std;
+// Return 1 = le dio a jugar
+// Return 2 = le dio a continuar
+// Return 3 = le dio a salir
+// Return 0 = error en algo
 int StartMenu()
 {
-    // Return 1 = le dio a jugar
-    // Return 2 = le dio a continuar
-    // Return 3 = le dio a salir
-    // Return 0 = error en algo
-    return 1;
+    int option;
+
+    cout << "====== MENU PRINCIPAL ======\n";
+    cout << "1. Jugar\n";
+    cout << "2. Continuar\n";
+    cout << "Seleccione una opción: ";
+    cin >> opcion;
+
+    if (opcion == 1)
+        return 1;
+    else if (opcion == 2)
+        return 2;
+    else
+        return 0;
+
 }
