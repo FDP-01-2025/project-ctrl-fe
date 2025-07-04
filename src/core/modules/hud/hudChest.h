837c0cbae83b0a87648a1e2261451cb560afc6a5
AnderTurcios <00003925@uca.edu.sv>
2025-07-03 22:13:04 -0600

    :sparkles: Agregar HUD de la sala del cofre

---
+++ src/core/modules/hud/hudChest.h
@@ -0,0 +1,55 @@
+#ifndef HUD_CHEST_H
+#define HUD_CHEST_H
+
+// Include required libraries
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+// Include headers for used classes
+#include "utils/screen/colors.h"
+#include "utils/player/player.h"
+#include "utils/functions/utils.h"
+#include "utils/functions/toWstring.h"
+
+// Class to draw the HUD (HUD = info panel)
+class HUDChest
+{
+public:
+    Utils utils; // Instance of Utils class for utility functions
+    void SetCenteredOffset(int x) { hudOffsetX = x; }
+    // Get the width of the HUD
+    // 'override' means this replaces a method from the base class
+    int GetWidth() const
+    {
+        return 25;
+    }
+
+    // Function to draw the HUD on the screen
+    // Takes the player, current level, and optional map width
+    void DrawLampEvent(Player player, int currentLevel, int mapWidth = 0)
+    {
+        utils.ClearScreen();                  // Clear the screen before drawing the HUD
+        int hudX = hudOffsetX + mapWidth + 3; // X position of the HUD
+        int totalLines = 10;
+        int consoleHeight = utils.GetConsoleHeight();
+        int y = ((consoleHeight - totalLines) / 3) + 5;
+        if (y < 0)
+            y = 0;
+
+        std::wcout << L"\033[" << y << ";" << hudX << L"H" << std::wstring(25, ' ');
+        utils.PrintLine(hudX, y, L"♥ - " + ToWString(player.GetLives()), RED);
+        y++;
+
+        // Controls
+        utils.PrintLine(hudX, y, L"Controls:", WHITE_BRIGHT);
+        utils.PrintLine(hudX, y, L"[WASD]: Move");
+
+        std::wcout << RESET; // Reset text color
+    }
+
+private:
+    int hudOffsetX = 0; // Offset for centering the HUD horizontally
+};
+
+#endif
